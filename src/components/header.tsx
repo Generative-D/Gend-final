dd5d0cbbe778cf09ebc288cf9dfe1c9920999c23
LeeJiho <leejiho35@gmail.com>
2024-06-04 17:56:51 +0900

    :sparkles: : add algokit sdk

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,58 +2,58 @@ import styled from "@emotion/styled/macro";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import tw from "twin.macro";
+import ConnectWallet from "./connect-wallet";
+import { useWallet } from "@txnlab/use-wallet";
 
 const Header = () => {
   const navigate = useNavigate();
-  const [isLogIn, setIsLogIn] = useState(false);
+  const [openWalletModal, setOpenWalletModal] = useState(false);
+  const toggleWalletModal = () => {
+    setOpenWalletModal((prev) => !prev);
+  };
+  const { activeAddress } = useWallet();
   return (
-    <Wrapper>
-      <HeaderBox>
-        <Title>Gen D</Title>
+    <>
+      <Wrapper>
+        <HeaderBox>
+          <Title>Gen D</Title>
 
-        <LogInBox>
-          {isLogIn ? (
-            <LogIn
-              onClick={() => {
-                setIsLogIn(false);
-              }}
-            >
-              LogIn
-            </LogIn>
-          ) : (
-            <LogOut
-              onClick={() => {
-                setIsLogIn(true);
-              }}
-            >
-              LogOut
-            </LogOut>
-          )}
-        </LogInBox>
-      </HeaderBox>
+          <LogInBox>
+            {activeAddress ? (
+              <LogOut>activeAddress</LogOut>
+            ) : (
+              <LogIn onClick={toggleWalletModal}>LogIn</LogIn>
+            )}
+          </LogInBox>
+        </HeaderBox>
 
-      <NavBox>
-        <NavItem
-          onClick={() => navigate("/")}
-          onNav={window.location.pathname === "/"}
-        >
-          Home
-        </NavItem>
+        <NavBox>
+          <NavItem
+            onClick={() => navigate("/")}
+            onNav={window.location.pathname === "/"}
+          >
+            Home
+          </NavItem>
 
-        <NavItem
-          onClick={() => navigate("/mine")}
-          onNav={window.location.pathname === "/mine"}
-        >
-          Mine
-        </NavItem>
-        <NavItem
-          onClick={() => navigate("/market")}
-          onNav={window.location.pathname === "/market"}
-        >
-          Market
-        </NavItem>
-      </NavBox>
-    </Wrapper>
+          <NavItem
+            onClick={() => navigate("/mine")}
+            onNav={window.location.pathname === "/mine"}
+          >
+            Mine
+          </NavItem>
+          <NavItem
+            onClick={() => navigate("/market")}
+            onNav={window.location.pathname === "/market"}
+          >
+            Market
+          </NavItem>
+        </NavBox>
+      </Wrapper>
+      <ConnectWallet
+        openModal={openWalletModal}
+        closeModal={toggleWalletModal}
+      />
+    </>
   );
 };
 
